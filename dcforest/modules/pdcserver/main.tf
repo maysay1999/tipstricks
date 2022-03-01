c6485599e2adbebcfb5be082a84c68b4b1cde3ac
Meisei Takemoto (NETAPP INC) <meisei@pobox.com>
2022-03-01 12:18:37 +0900

    ANF laboratory automatic creation ver 0.1

--- dcforest/modules/pdcserver/main.tf
+++ dcforest/modules/pdcserver/main.tf
@@ -290,7 +290,6 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.snet.id
     private_ip_address_allocation = var.private_ip_address_allocation_type
     private_ip_address            = var.private_ip_address_allocation_type == "Static" ? element(concat(var.private_ip_address, [""]), count.index) : null
-    public_ip_address_id          = var.enable_public_ip_address == true ? element(concat(azurerm_public_ip.pip.*.id, [""]), count.index) : null
   }
 }
 
